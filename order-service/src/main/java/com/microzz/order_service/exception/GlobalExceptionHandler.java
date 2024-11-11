35e7144dbec99c811c0490b17ba2b22d737d8e54
Sivareddy0930 <sivareddy0930@gmail.com>
2024-11-11 16:33:21 +0530

    Added Global Exception Handler

--- order-service/src/main/java/com/microzz/order_service/exception/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/microzz/order_service/exception/GlobalExceptionHandler.java
@@ -4,20 +4,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Date;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotExistWithIdException.class)
     public ResponseEntity<ExceptionResponse> userNotExist(UserNotExistWithIdException exception){
-        String timestamp = new java.util.Date().toString();
+        String timestamp = new Date().toString();
         ExceptionResponse response = new ExceptionResponse(timestamp,404, exception.getMessage());
         return ResponseEntity.badRequest().body(response);
     }
 
+    @ExceptionHandler(ServerUnavailableException.class)
+    public ResponseEntity<ExceptionResponse> serverUnavailableException(ServerUnavailableException serverUnavailableException){
+        String timestamp = new Date().toString();
+        ExceptionResponse response = new ExceptionResponse(timestamp,503, serverUnavailableException.getMessage());
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> globalExceptionHandler(Exception ex){
-        String timestamp = new java.util.Date().toString();
+        String timestamp = new Date().toString();
         ExceptionResponse response = new ExceptionResponse(timestamp,500, ex.getMessage());
         return ResponseEntity.badRequest().body(response);
     }
+
+
 }
